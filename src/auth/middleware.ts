b9bf0330ba55a9d7b5dc383bb9d1c8f69133a857
Colyn Brown <colyn.brown@gmail.com>
2025-02-12 18:41:49 -0500

    Use JWT only in middleware

--- src/auth/middleware.ts
+++ src/auth/middleware.ts
@@ -1,36 +1,36 @@
-// import { decodeJwt } from 'jose'
+import { decodeJwt } from 'jose'
 
 import { RequestCookies } from 'next/dist/compiled/@edge-runtime/cookies'
 import { NextRequest, NextResponse } from 'next/server'
 
-// const parseUserFromCookie = async (cookies: RequestCookies) => {
-//   const token = cookies.get('payload-token')?.value
-//   if (!token) return null
-
-//   const user = decodeJwt(token)
-//   if (!user) return null
-
-//   return user
-// }
-
-const parseUserFromEndpoint = async (cookies: RequestCookies) => {
+const parseUserFromCookie = async (cookies: RequestCookies) => {
   const token = cookies.get('payload-token')?.value
   if (!token) return null
 
-  const resp = await fetch('http://localhost:1111/api/users/me', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-  const user = await resp.json()
+  const user = decodeJwt(token)
   if (!user) return null
 
   return user
 }
 
+// const parseUserFromEndpoint = async (cookies: RequestCookies) => {
+//   const token = cookies.get('payload-token')?.value
+//   if (!token) return null
+
+//   const resp = await fetch('http://localhost:1111/api/users/me', {
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//     },
+//   })
+//   const user = await resp.json()
+//   if (!user) return null
+
+//   return user
+// }
+
 export const withAuth = async (request: NextRequest) => {
   const response = NextResponse.next()
-  const user = await parseUserFromEndpoint(request.cookies)
+  const user = await parseUserFromCookie(request.cookies)
   if (request.nextUrl.pathname.startsWith('/account') && !user) {
     return NextResponse.redirect(new URL('/', request.url))
   }
